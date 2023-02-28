c6a063a1c3c57b94ef5c26da68574835f0fb3b99
knhn1004 <kunhan945@gmail.com>
2023-02-28 15:10:37 -0800

    nvim init

---
+++ nvim/init.lua
@@ -0,0 +1,72 @@
+vim.cmd [[packadd packer.nvim]]
+
+-- Load plugins
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-surround'
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use 'lambdalisue/fern.vim'
+  use 'morhetz/gruvbox'
+  use 'tpope/vim-commentary'
+  use 'SirVer/ultisnips'
+
+end)
+
+
+-- Set options
+vim.cmd('set rnu nu')
+vim.cmd('syntax on')
+
+-- Remap leader to space
+vim.g.mapleader = ' '
+
+-- Fern setup
+vim.api.nvim_set_keymap('n', '<leader>f', ':Fern . -drawer<CR>', { noremap = true })
+
+-- Set the colorscheme to Gruvbox
+vim.cmd('colorscheme gruvbox')
+
+
+-- Set the color scheme to gruvbox
+vim.o.termguicolors = true
+
+-- Define a function to compile and run C++ files
+function CompileAndRunCpp()
+  -- Get the current buffer name
+  local filename = vim.fn.expand('%:p')
+
+  -- Compile the file using g++
+  local cmd = 'g++ -std=c++20 -O2 -Wall -Wextra -Wshadow -o a.out '
+    .. vim.fn.shellescape(filename)
+  vim.fn.system(cmd)
+
+  -- Run the compiled file in a new vertical split pane
+  vim.cmd('vsplit')
+  vim.cmd('terminal ./a.out')
+
+  -- Switch back to the original buffer
+  vim.api.nvim_command('wincmd h')
+end
+
+-- Bind the function to a key combination
+vim.api.nvim_set_keymap('n', '<C-A-n>', ':lua CompileAndRunCpp()<CR>', { noremap = true, silent = true })
+
+
+-- Set the prefix key for window commands to ctrl+hjkl
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+-- Configure Commentary plugin
+vim.g.commentary_line_mapping = "<C-_>"
+vim.g.commentary_register_keybindings = {
+  n = { "<leader>c", "<leader>" },
+  v = { "<leader>c", "<leader>" },
+}
+
+-- Enable commenting with Ctrl+/
+vim.api.nvim_set_keymap('n', '<C-_>', ':Commentary<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-_>', ':Commentary<CR>', { noremap = true, silent = true })
+
