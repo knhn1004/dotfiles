6b2d282a4f0be66446af56266043b76de2271a9d
knhn1004 <kunhan945@gmail.com>
2023-03-07 09:22:47 -0800

    udpate lsp conf

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -12,9 +12,6 @@ lsp.ensure_installed({
   'gopls',
 })
 
-
-
-
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -46,7 +43,7 @@ vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end)
 -- vim.keymap.set("n", "<leader><C-.>", function() vim.lsp.buf.code_action() end)
 vim.keymap.set("n", "ga", function() vim.lsp.buf.code_action() end)
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -55,9 +52,6 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  -- vim.keymap.set("n", "<C-.>", function() vim.lsp.buf.code_action() end, opts)
-
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
