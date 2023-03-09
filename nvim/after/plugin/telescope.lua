09f1f3beac265e29d2c209331a06101c33000abd
knhn1004 <kunhan945@gmail.com>
2023-03-09 10:46:59 -0800

    lazygit.nvim colortheme

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,5 +2,11 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ")})
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+vim.api.nvim_command('autocmd BufEnter * :lua require("lazygit.utils").project_root_dir()')
+
+-- Add keymapping for lazygit command
+vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>lua require("telescope").extensions.lazygit.lazygit()<CR>',
+  { noremap = true, silent = true })
--- nvim/remap.lua
+++ nvim/remap.lua
@@ -20,7 +20,7 @@ function CompileAndRunCpp()
 
   -- Compile the file using g++
   local cmd = 'g++ -std=c++20 -O2 -Wall -Wextra -Wshadow -o a.out '
-    .. vim.fn.shellescape(filename)
+      .. vim.fn.shellescape(filename)
   vim.fn.system(cmd)
 
   -- Run the compiled file in the same buffer
@@ -30,11 +30,9 @@ function CompileAndRunCpp()
   vim.api.nvim_command('wincmd h')
 end
 
-
 -- Bind the function to a key combination
 vim.api.nvim_set_keymap('n', '<C-A-n>', ':lua CompileAndRunCpp()<CR>', { noremap = true, silent = true })
 
 
 -- remove the default <C-W> mapping to avoid bad habits on browsers
 vim.api.nvim_set_keymap('i', '<C-W>', '<Nop>', { noremap = true })
-
